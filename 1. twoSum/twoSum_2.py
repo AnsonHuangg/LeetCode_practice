78da6a3e94dd23e11ac2b70f61afd5648055dafd
AnsonHuangg <qo602@yahoo.com.tw>
2019-12-26 19:32:36 +0800

    Add another solution for twoSum

---
+++ 1. twoSum/twoSum_2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i=0
+        while i<len(nums):
+            if target - nums[i] in nums[i+1:]:
+                if target - nums[i] != nums[i]:
+                    return str(i),str(nums.index(target - nums[i]))
+                    break
+                else:
+                    return str(i),str(nums.index(target-nums[i],i+1,len(nums)))
+                    break
+            else:
+                i += 1
