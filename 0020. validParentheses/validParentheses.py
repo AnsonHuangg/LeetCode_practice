a1559e069930af4bebbbcaf95b950d7c2121533e
AnsonHuangg <qo602@yahoo.com.tw>
2020-01-03 16:31:01 +0800

    Update Valid Parentheses

--- 0020. validParentheses/validParentheses.py
+++ 0020. validParentheses/validParentheses.py
@@ -1,25 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #sample = ['(', ')', '{', '}', '[',']']
         stack = []
         while s!='':
             if s[0]=='('or s[0]=='{'or s[0]=='[':
                 stack.append(s[0])
-            elif s[0]==[]:
+            elif stack==[]:
                 return False
                 break
             elif s[0]==')' and stack[-1]=='(':
-                del stack[-1]
+                stack.pop()
             elif s[0]=='}' and stack[-1]=='{':
-                del stack[-1]
+                stack.pop()
             elif s[0]==']' and stack[-1]=='[':
-                del stack[-1]
+                stack.pop()
             else:
                 return False
                 break
             s = s[1:]
         if stack==[]:
             return True
-        
-
-Solution().isValid(']')
