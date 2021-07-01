043424c484f2412f78d2d7a39963b4a784ebeb07
AnsonHuangg <qo602@yahoo.com.tw>
2021-07-01 22:43:14 +0800

    Add third solution

---
+++ 1480. runningSum/runningSum_3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        i = 1
+        while i < len(nums):
+            nums[i] = nums[i]+nums[i-1]
+            i += 1
+        return nums
